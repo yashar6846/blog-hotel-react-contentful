b74ba1b95ad78ca31cf2fa32367e9d04749d786e
yashar6846 <yasharnajafi6846@gmail.com>
2021-11-03 00:03:27 +0300

    add styled component

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,15 +2,19 @@ import React from "react";
 import Hero from "../component/Hero";
 import Banner from "../component/Banner";
 import { Link } from "react-router-dom";
+import RoomContainer from "../component/RoomContainer";
 
 export default function Rooms() {
   return (
-    <Hero hero="roomsHero">
-      <Banner title="our room">
-        <Link to="/" className="btn-primary">
-          return home
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero hero="roomsHero">
+        <Banner title="our room">
+          <Link to="/" className="btn-primary">
+            return home
+          </Link>
+        </Banner>
+      </Hero>
+      <RoomContainer />
+    </>
   );
 }
