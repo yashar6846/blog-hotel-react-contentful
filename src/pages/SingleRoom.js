d23000527c17918d05a51ff42ad1408578effc01
yashar6846 <yasharnajafi6846@gmail.com>
2021-11-02 17:18:02 +0300

    add rooms

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
+import defaultBcg from "../images/room-1.jpeg";
+import Hero from "../component/Hero";
+import Banner from "../component/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
 
-export default function SingleRoom() {
-  return (
-    <div>
-      <>SingleRoom</>
-    </div>
-  );
+export default class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+    // console.log(this.props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg,
+    };
+  }
+  static contextType = RoomContext;
+  // componentDidMount() {}
+  render() {
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>no such room could be found</h3>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </div>
+      );
+    }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images,
+    } = room;
+    return (
+      <Hero hero="roomsHero">
+        <Banner title={`${name} room`}>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </Banner>
+      </Hero>
+    );
+  }
 }
