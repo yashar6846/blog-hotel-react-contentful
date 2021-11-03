78247fee05cbfb17432c17458b5f51924bdbae60
yashar6846 <yasharnajafi6846@gmail.com>
2021-11-03 12:59:10 +0300

    adding contex and project hotel

--- src/component/RoomContainer.js
+++ src/component/RoomContainer.js
@@ -1,25 +1,47 @@
 import React from "react";
 import RoomsFilter from "./RoomsFilter";
 import RoomsList from "./RoomsList";
-import { RoomConsumer } from "../context";
+import { withRoomConsumer } from "../context";
 import Loading from "./Loading";
 
-export default function RoomContainer() {
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
   return (
-    <RoomConsumer>
-      {(value) => {
-        const { loading, sortedRooms, rooms } = value;
-        if (loading) {
-          return <Loading />;
-        }
-        return (
-          <div>
-            Hello Froms Rooms RoomsContainer
-            <RoomsFilter rooms={rooms} />
-            <RoomsList rooms={sortedRooms} />
-          </div>
-        );
-      }}
-    </RoomConsumer>
+    <>
+      <RoomsFilter rooms={rooms} />
+      <RoomsList rooms={sortedRooms} />
+    </>
   );
 }
+
+export default withRoomConsumer(RoomContainer);
+
+// import React from "react";
+// import RoomsFilter from "./RoomsFilter";
+// import RoomsList from "./RoomsList";
+// import { RoomConsumer } from "../context";
+// import Loading from "./Loading";
+// import RoomContainer from "./RoomContainer";
+
+// export default function RoomContainer() {
+//   return (
+//     <RoomConsumer>
+//       {(value) => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+//         return (
+//           <div>
+//             Hello Froms Rooms RoomsContainer
+//             <RoomsFilter rooms={rooms} />
+//             <RoomsList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// }
--- src/component/RoomsFilter.js
+++ src/component/RoomsFilter.js
@@ -1,5 +1,54 @@
 import React from "react";
-
-export default function RoomsFilter() {
-  return <div>hello from room filter</div>;
+import { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "../component/Title";
+//get all unique values
+const getUnique = (items, value) => {
+  return [...new Set(items.map((item) => item[value]))];
+};
+export default function RoomsFilter({ rooms }) {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+  } = context;
+  //get unique types
+  let types = getUnique(rooms, "type");
+  //add all
+  types = ["all", ...types];
+  //map to jsx
+  types = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <from className="filter-form">
+        <div className="form-group">
+          <lable htmlFor="type">room type</lable>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+      </from>
+    </section>
+  );
 }
